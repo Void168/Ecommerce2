bcb427c42d324dd35886c3fb2a40695ecc9078e0
LPHung <phuhung16820@gmail.com>
2023-02-18 23:03:26 +0700

    responsive footer

--- frontend/ecommerce2/src/components/Footer.jsx
+++ frontend/ecommerce2/src/components/Footer.jsx
@@ -14,43 +14,76 @@ function Footer() {
           : "bg-[#D8E3E7]"
       }
     >
-      <div className="container mx-auto">
+      <div className="container mx-auto p-4">
         <div>
-          <div xl={3} lg={3} md={12}>
-            <h2>
-              CÔNG TY CỔ PHẦN HFOOD © 2023 - 2100 Công Ty Cổ Phần Thương Mại
-              WEIRDSHOP
-            </h2>
-            <h3>
+          <div>
+            <p className="text-2xl">
+              CÔNG TY CỔ PHẦN WEIRDSHOP © 2023 - 2100 Công Ty Cổ Phần Thương Mại
+              weirdshop
+            </p>
+            <p className="text-lg my-4">
               Giấy chứng nhận đăng ký doanh nghiệp: 0280920130 do Sở KH-ĐT
               TP.HCM cấp lần đầu ngày 16 tháng 08 năm 2023
-            </h3>
+            </p>
             <br />
-            <h4>Chính sách vận chuyển</h4>
-            <h4>Chăm Sóc Khách Hàng</h4>
-            <h4>Thanh Toán</h4>
+            <div className="grid big-tablet:grid-cols-3 small-phone:grid-cols-1 gap-8">
+              <div className="grid grid-cols-3 big-phone:col-span-2 small-phone:col-span-3 gap-4 tablet:text-base small-phone:text-sm">
+                <div>
+                  <p>Chính sách vận chuyển</p>
+                  <p>Chăm Sóc Khách Hàng</p>
+                  <p>Thanh Toán</p>
+                </div>
+                <div>
+                  <p>Chính sách vận chuyển</p>
+                  <p>Chăm Sóc Khách Hàng</p>
+                  <p>Thanh Toán</p>
+                </div>
+                <div>
+                  <p>Chính sách vận chuyển</p>
+                  <p>Chăm Sóc Khách Hàng</p>
+                  <p>Thanh Toán</p>
+                </div>
+                <div>
+                  <p>Chính sách vận chuyển</p>
+                  <p>Chăm Sóc Khách Hàng</p>
+                  <p>Thanh Toán</p>
+                </div>
+                <div>
+                  <p>Chính sách vận chuyển</p>
+                  <p>Chăm Sóc Khách Hàng</p>
+                  <p>Thanh Toán</p>
+                </div>
+                <div>
+                  <p>Chính sách vận chuyển</p>
+                  <p>Chăm Sóc Khách Hàng</p>
+                  <p>Thanh Toán</p>
+                </div>
+              </div>
+              <div className="big-phone:col-span-1 small-phone:col-span-3 flex flex-col justify-center big-phone:p-0 small-phone:px-4">
+                <p>
+                  Địa chỉ trụ sở chính: 279 Nguyễn Tri Phương, phường 5, quận
+                  10, TP. Hồ Chí Minh
+                </p>
+                <p>
+                  Văn phòng điều hành miền Bắc: Tầng 6, Số 1 Phố Mai Động,
+                  Phường Kim Ngưu, Quận Hoàng Mai, Hà Nội
+                </p>
+                <p>
+                  Văn phòng điều hành miền Nam: Tầng 3, số 5/3 Đồ Sơn, Phường 4,
+                  Quận Tân Bình, TP. Hồ Chí Minh
+                </p>
+                <br />
+                <br />
+              </div>
+            </div>
+
             <br />
           </div>
-          <div xl={4} lg={4} md={12}>
-            <h3>
-              Địa chỉ trụ sở chính: 279 Nguyễn Tri Phương, phường 5, quận 10,
-              TP. Hồ Chí Minh
-              <br />
-              Văn phòng điều hành miền Bắc: Tầng 6, Số 1 Phố Mai Động, Phường
-              Kim Ngưu, Quận Hoàng Mai, Hà Nội
-              <br />
-              Văn phòng điều hành miền Nam: Tầng 3, số 5/3 Đồ Sơn, Phường 4,
-              Quận Tân Bình, TP. Hồ Chí Minh
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <strong>
-                &emsp; &emsp; &emsp; &emsp; All rights reserved &emsp; &emsp;
-                &emsp; &emsp; CopyRight © 2023
-              </strong>
-            </h3>
+          <div>
+            <div class="flex big-phone:flex-row small-phone:flex-col text-center justify-evenly big-tablet:m-0 small-phone:mb-20 text-xl">
+              <p>All rights reserved</p>
+              <p>CopyRight © 2023</p>
+            </div>
             <br />
           </div>
           <div
