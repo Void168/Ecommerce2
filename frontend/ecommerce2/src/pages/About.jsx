86a71d10dde28c1ea06712775cdca7aa15c8f12c
LPHung <phuhung16820@gmail.com>
2023-04-03 10:48:16 +0700

    responsive about page

--- frontend/ecommerce2/src/pages/About.jsx
+++ frontend/ecommerce2/src/pages/About.jsx
@@ -33,7 +33,7 @@ function About() {
                   Giới thiệu WeirdShop
                 </p>
                 <div className="flex flex-col items-end">
-                  <div className="mx-72 bg-black bg-opacity-70 p-12">
+                  <div className="big-desktop:mx-72 laptop:mx-48 big-tablet:mx-24 bg-black bg-opacity-70 p-12">
                     <p className="leading-8">
                       Công Ty Cổ Phần Thương Mại Dịch Vụ WeirdShop Xuất thân từ
                       cửa hàng kinh doanh máy tính được thành lập từ năm 2023,
@@ -80,16 +80,16 @@ function About() {
             </div>
             <div className="flex flex-row justify-between my-20">
               <div
-                className="bg-cover text-white p-8 w-7/12 rounded-br-full shadow-sm"
+                className="bg-cover text-white p-8 big-tablet:w-7/12 small-phone:w-10/12 rounded-br-full shadow-sm"
                 style={{
                   backgroundImage: `url('https://c4.wallpaperflare.com/wallpaper/744/804/845/computer-engineering-science-tech-wallpaper-preview.jpg')`,
                 }}
-                data-aos="fade-up-right"
+                data-aos="fade-up"
               >
                 <p className="text-4xl my-8">Hành trình phát triển</p>
                 <div className="flex flex-col items-end">
-                  <div className="mx-16 bg-black bg-opacity-70 w-10/12 p-4">
-                    <p className="leading-8">
+                  <div className="mx-8 bg-black bg-opacity-70 w-10/12 p-4">
+                    <p className="leading-8 laptop:text-base big-tablet:text-sm small-phone:text-base">
                       1997: Bắt đầu từ một cửa hàng nhỏ bán lắp ráp phần cứng
                       máy tính & PC tại TP HCM. 2000: Khai trương trung tâm dịch
                       vụ hàng đầu tại TP.HCM 2007: Công ty Thương mại & Dịch vụ
@@ -107,30 +107,30 @@ function About() {
                 </div>
               </div>
               <img
-                className="mx-16 my-8 w-5/12 max-h-96"
+                className="mx-8 my-8 w-5/12 max-h-96 big-tablet:block small-phone:hidden"
                 src="https://c0.wallpaperflare.com/preview/1007/355/640/industry-industry-4-web-network.jpg"
                 alt="development"
-                data-aos="fade-up-left"
+                data-aos="fade-up"
               />
             </div>
-            <div className="flex flex-row items-end my-20">
+            <div className="flex flex-row items-end my-20 small-phone:justify-end big-tablet:justify-between">
               <img
-                className="mx-16 my-8 w-5/12 max-h-96"
+                className="mx-8 my-8 w-5/12 max-h-96 big-tablet:block small-phone:hidden"
                 src="https://e0.pxfuel.com/wallpapers/328/77/desktop-wallpaper-services-business-success.jpg"
                 alt="development"
-                data-aos="fade-up-right"
+                data-aos="fade-up"
               />
               <div
-                className="bg-cover text-white p-8 w-7/12 rounded-bl-full shadow-sm"
+                className="bg-cover text-white p-8 big-tablet:w-7/12 small-phone:w-10/12 rounded-bl-full shadow-sm"
                 style={{
                   backgroundImage: `url('https://c4.wallpaperflare.com/wallpaper/744/804/845/computer-engineering-science-tech-wallpaper-preview.jpg')`,
                 }}
-                data-aos="fade-up-left"
+                data-aos="fade-up"
               >
                 <p className="text-4xl my-8 text-center">Lĩnh vực kinh doanh</p>
                 <div className="flex flex-col items-end">
-                  <div className="mx-16 bg-black bg-opacity-70 w-9/12 p-4">
-                    <p className="leading-8">
+                  <div className="mx-8 bg-black bg-opacity-70 big-tablet:w-9/12 small-phone:w-10/12 p-4">
+                    <p className="leading-8 laptop:text-base big-tablet:text-sm small-phone:text-base">
                       WeirdShop cung cấp đầy đủ các sản phẩm máy tính, thiết bị
                       công nghệ thông tin & hi-end cho người dùng gia đình Cung
                       cấp giải pháp tích hợp hệ thống công nghệ thông tin cho
@@ -152,16 +152,16 @@ function About() {
 
             <div className="flex flex-row items-start my-12">
               <div
-                className="bg-cover text-white p-8 w-7/12 rounded-br-full shadow-sm"
+                className="bg-cover text-white p-8 big-tablet:w-7/12 small-phone:w-10/12 rounded-br-full shadow-sm"
                 style={{
                   backgroundImage: `url('https://c4.wallpaperflare.com/wallpaper/744/804/845/computer-engineering-science-tech-wallpaper-preview.jpg')`,
                 }}
-                data-aos="fade-up-right"
+                data-aos="fade-up"
               >
                 <p className="text-4xl my-8">Thành tựu</p>
                 <div className="flex flex-col items-end">
-                  <div className="mx-16 bg-black bg-opacity-70 w-10/12 p-4">
-                    <p className="leading-8">
+                  <div className="mx-8 bg-black bg-opacity-70 w-10/12 p-4">
+                    <p className="leading-8 laptop:text-base big-tablet:text-sm small-phone:text-base">
                       Bằng những nỗ lực không mệt mỏi, WeirdShop tự hào là nhà
                       bán lẻ thiết bị công nghệ cao được khách hàng và các đối
                       tác tin tưởng và yêu mến. Đó là giải thưởng và niềm tự hào
@@ -177,10 +177,10 @@ function About() {
                 </div>
               </div>
               <img
-                className="mx-16 my-8 w-5/12 max-h-96"
+                className="mx-8 my-8 w-5/12 max-h-96 big-tablet:block small-phone:hidden"
                 src="https://img.freepik.com/free-vector/achievement-quotes-with-success-medal-goal-symbols-realistic-illustration_1284-28111.jpg"
                 alt="development"
-                data-aos="fade-up-left"
+                data-aos="fade-up"
               />
             </div>
           </div>
